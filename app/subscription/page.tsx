b0d11e62a075f8354deebb82334fdc47ba250514
yu <douzhaoyu@gmail.com>
2025-07-23 23:30:21 -0700

    implement the subscription feature

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { PricingTable } from '@clerk/nextjs';
 
 const  Subsription = () => {
   return (
-    <div>Subsription</div>
+    <div><PricingTable /></div>
   )
 }
 
