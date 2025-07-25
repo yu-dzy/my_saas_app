240999202ceae202f1d55bf2b7a55af428e55458
yu <douzhaoyu@gmail.com>
2025-07-24 22:09:03 -0700

    Implemented database setup and created companion server action

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import CompanionForm from '@/components/CompanionForm'
-const NewCompanion = () => {
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const NewCompanion = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in"); // or your custom sign-in page
+  }
   return (
     <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
       <article className='w-full gap-4 flex flex-col'>
--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -23,7 +23,7 @@ import {
 } from "@/components/ui/select"
 import {subjects} from "@/constants";
 import {Textarea} from "@/components/ui/textarea";
-// import {createCompanion} from "@/lib/actions/companion.actions";
+import {createCompanion} from "@/lib/actions/companion.actions";
 import {redirect} from "next/navigation";
 
 const formSchema = z.object({
