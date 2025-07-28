414a16da1b88c05de613a3a41a38162e56b48c7b
yu <douzhaoyu@gmail.com>
2025-07-27 17:04:03 -0700

    Implemented the vapi and functions of component

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -38,3 +38,15 @@ export const getAllCompanions = async ({ limit = 10, page = 1, subject, topic }:
 
     return companions;
 }
+export const getCompanion = async (id: string) => {
+    const supabase = createSupabaseClient();
+
+    const { data, error } = await supabase
+        .from('companions')
+        .select()
+        .eq('id', id);
+
+    if(error) return console.log(error);
+
+    return data[0];
+}
