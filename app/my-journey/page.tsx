bffc53567c38897578c6d4f044b9537a4e23cb2e
yu <douzhaoyu@gmail.com>
2025-07-29 21:39:31 -0700

    profile updated

--- app/my-journey/page.tsx
+++ app/my-journey/page.tsx
@@ -14,89 +14,22 @@ import {
 import Image from "next/image";
 import CompanionsList from "@/components/CompanionsList";
 
-const Profile = async () => {
-  const user = await currentUser();
-
-  if (!user) redirect("/sign-in");
-
-  const companions = await getUserCompanions(user.id);
-  const sessionHistory = await getUserSessions(user.id);
-  const bookmarkedCompanions = await getBookmarkedCompanions(user.id);
-
+const Profile = () => {
   return (
     <main className="min-lg:w-3/4">
-      <section className="flex justify-between gap-4 max-sm:flex-col items-center">
-        <div className="flex gap-4 items-center">
-          <Image
-            src={user.imageUrl}
-            alt={user.firstName!}
-            width={110}
-            height={110}
-          />
-          <div className="flex flex-col gap-2">
-            <h1 className="font-bold text-2xl">
-              {user.firstName} {user.lastName}
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              {user.emailAddresses[0].emailAddress}
-            </p>
-          </div>
-        </div>
-        <div className="flex gap-4">
-          <div className="border border-black rouded-lg p-3 gap-2 flex flex-col h-fit">
-            <div className="flex gap-2 items-center">
-              <Image
-                src="/icons/check.svg"
-                alt="checkmark"
-                width={22}
-                height={22}
-              />
-              <p className="text-2xl font-bold">{sessionHistory.length}</p>
-            </div>
-            <div>Lessons completed</div>
-          </div>
-          <div className="border border-black rouded-lg p-3 gap-2 flex flex-col h-fit">
-            <div className="flex gap-2 items-center">
-              <Image src="/icons/cap.svg" alt="cap" width={22} height={22} />
-              <p className="text-2xl font-bold">{companions.length}</p>
-            </div>
-            <div>Companions created</div>
-          </div>
-        </div>
+      <section>
       </section>
-      <Accordion type="multiple">
-        <AccordionItem value="bookmarks">
-          <AccordionTrigger className="text-2xl font-bold">
-            Bookmarked Companions {`(${bookmarkedCompanions.length})`}
-          </AccordionTrigger>
+
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
           <AccordionContent>
-            <CompanionsList
-              companions={bookmarkedCompanions}
-              title="Bookmarked Companions"
-            />
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="recent">
-          <AccordionTrigger className="text-2xl font-bold">
-            Recent Sessions
-          </AccordionTrigger>
-          <AccordionContent>
-            <CompanionsList
-              title="Recent Sessions"
-              companions={sessionHistory}
-            />
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="companions">
-          <AccordionTrigger className="text-2xl font-bold">
-            My Companions {`(${companions.length})`}
-          </AccordionTrigger>
-          <AccordionContent>
-            <CompanionsList title="My Companions" companions={companions} />
+            Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
         </AccordionItem>
       </Accordion>
     </main>
   );
 };
+
 export default Profile;
